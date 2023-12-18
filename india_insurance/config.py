4e69c454db1946fa50053e6e67fb016381eb3661
chaitsugale <ugalechaitanya80@gmail.com>
2023-12-18 17:50:52 +0530

    Added config for mongodb and new artifacts class

--- india_insurance/config.py
+++ india_insurance/config.py
@@ -1,5 +1,18 @@
 import pandas as pd
+import pymongo
+import json
+import os
 
 from dataclasses import dataclass
+
+class EnvironmentVariable:
+    mongo_db_url:str = os.getenv("MONGO_DB_URL")
+    #mongo_db_url = 'mongodb://localhost:27017'
+    
+
+env_var = EnvironmentVariable()
+
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+print(env_var.mongo_db_url)
 
 TARGET_COLUMN = "expenses"
---
+++ india_insurance/entity/artifact_entity.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    feature_store_file_path:str
+    train_data_path:str
+    test_data_path:str
+
+@dataclass
+class DataValidationArtifact:
+    report_file_path:str
+
+@dataclass
+class DataTransformationArtifact:
+    transform_object_path:str
+    transformed_train_path:str
+    transformed_test_path:str
+    target_encoded_path:str
+
+@dataclass
+class ModelTraninerArtifact:
+    model_path:str
+    r2_train_score:float
+    r2_test_score:float
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted:bool
+    improved_accuracy:float
+
+@dataclass
+class ModelValidationArtifact:
+    pusher_model_dir:str
+    saved_model_dir:str
