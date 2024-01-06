9d12e6ab705f53d9320e949fcd0ae65e4b74a93a
chaitsugale <ugalechaitanya80@gmail.com>
2024-01-06 12:24:40 +0530

    web page building in progress

--- app.py
+++ app.py
@@ -1,44 +1,15 @@
-from flask import Flask,request,render_template
-import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
 import numpy as np
-from india_insurance.pipeline.predict_pipeline import CustomData,PredictPipeline
-
+import pandas as pd
+import seaborn as sns
+import pickle
+import xgboost as xg
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 
-application = Flask(__name__)
+india_insurance_model = pickle.load(open('india_insurance_best_model.pkl','rb'))
+india_insurance_target_encoder = pickle.load(open('india_insurance_target_encoder.pkl','rb'))
+india_insurance_transformer = pickle.load(open('india_insurance_transformer.pkl','rb'))
 
-app = application
-
-## Route for homepage
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/predictdata',methods= ['GET','POST'])
-
-def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
-    else:
-        data = CustomData(
-            age = request.form.get('age'), 
-            sex = request.form.get('sex'),
-            bmi = request.form.get('bmi'),
-            children = request.form.get('children'),
-            smoker =  request.form.get('smoker'),
-            region  = request.form.get('region'),
-            expenses  = request.form.get('expenses'),
-        )
-
-        pred_df = data.get_data_as_dataframe()
-        print(pred_df)
-
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-        return render_template('home.html',results = results[0])
-    
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0",debug = True)
-    
+st.title("India Insurance Predictor")
