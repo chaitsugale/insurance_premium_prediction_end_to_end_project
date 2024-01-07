1b22138adf5ec06cc65046543f3faff990fd2f78
chaitsugale <ugalechaitanya80@gmail.com>
2024-01-07 16:48:07 +0530

    change_2

--- india_insurance/config.py
+++ india_insurance/config.py
@@ -7,7 +7,10 @@ from dataclasses import dataclass
 
 class EnvironmentVariable:
     #mongo_db_url:str = os.getenv("MONGO_DB_URL")
-    mongo_db_url = 'mongodb://localhost:27017'
+    #mongo_db_url = 'mongodb://localhost:27017'
+    mongo_db_url:str = os.getenv("MONGO_DB_URL")
+    aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_access_secret_key:str = os.getenv("AWS_SECRET_ACCESS_KEY")
     
 
 env_var = EnvironmentVariable()
